41d8ed73aba271ff57df752148945f4b22752ead
Ali <varena1383@gmail.com>
2025-08-22 19:05:40 +0330

    [Add] Design home header

--- Components/Shop.jsx
+++ Components/Shop.jsx
@@ -7,26 +7,28 @@ import { Table, Container, Spinner } from "react-bootstrap";
 const Shop = () => {
   const [data, setData] = useState([]);
   const Connection = Networkstatus();
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if (Connection) {
+    if (loading) {
       fetch("https://jsonplaceholder.typicode.com/posts")
         .then((res) => res.json())
         .then((data) => {
           setData(data);
-          setTimeout(() => {
-            setLoading(false);
-          }, 3000);
+          // setTimeout(() => {
+          //   setLoading(false);
+          // }, 3000);
         })
         .catch(() => setLoading(false));
     }
-  }, [Connection]);
+  }, []);
 
   window.addEventListener("load", function () {
     console.log("loading...");
-
-    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+    // setLoading(false);
   });
 
   return (
@@ -34,13 +36,13 @@ const Shop = () => {
       {Connection ? (
         <Container dir="ltr">
           <h1>Shop</h1>
-
           {loading ? (
             <div
               className="d-flex justify-content-center align-items-center"
-              style={{ height: "200px" }}
+              style={{ height: "300px" }}
             >
               <Spinner animation="border" role="status" />
+              {/* <span className="ms-2">درحال انتفال به سایت مورد نظر هستید...</span> */}
               <span className="ms-2">Loading...</span>
             </div>
           ) : (
